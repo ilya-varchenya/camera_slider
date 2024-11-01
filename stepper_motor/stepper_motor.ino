27c5147f98e8c7450ab9eecee82e490afe1b30ac
ilya-varchenya <ilya.varchenya@gmail.com>
2024-11-01 20:02:50 +0200

    2 motors are running simultaneously

--- stepper_motor/stepper_motor.ino
+++ stepper_motor/stepper_motor.ino
@@ -6,9 +6,14 @@
 #define STEP_PIN_2 5
 #define DIR_PIN_2  7
 
+#define DEG_PER_STEP 9
+
 // EN_PIN_*, STEP_PIN_*, DIR_PIN_*
 int Stepper1[] = {2, 4, 6};
 int Stepper2[] = {3, 5, 7};
+
+unsigned long startMillis;
+unsigned long startMillisStep;
 
 void setup() {
   Serial.begin(9600);
@@ -17,65 +22,42 @@ void setup() {
   pinMode(Stepper1[1], OUTPUT);
   pinMode(Stepper2[0], OUTPUT);
   pinMode(Stepper2[1], OUTPUT);
+  // disable drivers by default
+  digitalWrite(Stepper1[0], HIGH);
+  digitalWrite(Stepper2[0], HIGH);
+
+  startMillis = millis();
+  startMillisStep = millis();
 }
 
 void loop() {
   // move 1 motor
   // moveMotor(Stepper1, 180, 2, 1);
-  // moveMotor(Stepper2, 180, 2, 1);
-
-
-  moveMotorS(Stepper1,
-            180,
-            1,
-            Stepper2,
-            180,
-            0,
-            2 // time in seconds
-          );
-
-  delay(3000);
-}
-
-void moveMotorS(int Stepper1[], int angle1, int direction1, int Stepper2[], int angle2, int direction2, int time) {
-  digitalWrite(Stepper1[0], LOW);         // Enable driver 1
-  digitalWrite(Stepper2[0], LOW);         // Enable driver 2
-  digitalWrite(Stepper1[2], direction1);  // Set direction 1
-  digitalWrite(Stepper2[2], direction2);  // Set direction 2
+  moveMotor(Stepper2, 720, 2, 1);
   
-
-  int deg_per_step = 9;
-  int steps1 = angle1 * deg_per_step;
-  int steps2 = angle2 * deg_per_step;
-  float delay_time1 = (time * 1000/ steps1);
-  float delay_time2 = (time * 1000/ steps2);
-
-  for (int i = 1; i <= steps; i++) {
-    digitalWrite(Stepper[1], HIGH);  // Step
-    delay(delay_time);
-    digitalWrite(Stepper[1], LOW);
-    delay(delay_time);
-  }
-
-  digitalWrite(Stepper[0], HIGH);  // Disable driver
+  // delay(3000);
 }
 
 
 // 200 steps * 16 micro steps = 3200 steps
 void moveMotor(int Stepper[], int angle, int time, int direction) {
-  digitalWrite(Stepper[0], LOW);       // Enable driver
-  digitalWrite(Stepper[2], direction); // Set direction: 0 - left, 1 - right
+  float period = (time * 1000 * 2 / (angle * DEG_PER_STEP));
+  float finish_time = time * 1000;
 
-  int deg_per_step = 9;
-  int steps = angle * deg_per_step;
-  float delay_time = (time * 1000/ steps);
-
-  for (int i = 1; i <= steps; i++) {
-    digitalWrite(Stepper[1], HIGH);  // Step
-    delay(delay_time);
-    digitalWrite(Stepper[1], LOW);
-    delay(delay_time);
+  if (millis() - startMillis <= finish_time) {
+    Serial.println("1");
+    digitalWrite(Stepper[0], LOW);       // Enable driver
+    digitalWrite(Stepper[2], direction); // Set direction: 0 - left, 1 - right
+    if (millis() - startMillisStep >= period) {
+      Serial.println("2");
+      digitalWrite(Stepper[1], !digitalRead(Stepper[1]));  // Step
+      startMillisStep = millis();
+    }
+  } else {
+    Serial.println("3");
+    digitalWrite(Stepper[0], HIGH);  // Disable driver
+    delay(3000);
+    startMillis = millis();
+    startMillisStep = millis();
   }
-
-  digitalWrite(Stepper[0], HIGH);  // Disable driver
 }
